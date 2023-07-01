5b600c5379aaae694570233a1164a5adb2616b27
Aleksy <psychologianauki@gmail.com>
2023-07-01 11:17:18 +0200

    fixed routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,9 @@
-// import { useRouter } from "next/router"
-// import { routes } from "@/routes"
-// import Layout from "@/Components/Layout/Layout"
-// import AboutPage from "./about/index"
-// import HomePage from "./home"
-
-// export default function Home() {
-//   const router = useRouter()
-//   const getComponentToRender = () => {
-//     switch (router.pathname) {
-//       case "/":
-//         return <HomePage />
-//       case "/home":
-//         return <HomePage />
-//       case "/about":
-//         return <AboutPage />
-//       default:
-//         return <HomePage />
-//     }
-//   }
-
-//   console.error(router.pathname)
-//   console.error(routes.ABOUTUS)
-//   return <Layout>{getComponentToRender()}</Layout>
-// }
+import React from "react"
+import Home from "./home/index"
+export default function index() {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
