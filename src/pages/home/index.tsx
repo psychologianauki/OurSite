8850c8b020d77aa2f82ef4d75d87b78bd54ed01a
Aleksy <psychologianauki@gmail.com>
2023-07-01 19:37:07 +0200

    work in progress

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,14 +4,27 @@ import React from "react"
 import MovingBlob from "@/Components/MovingBlob/MovingBlob"
 import ProgrammingIcon from "@/icons/ProgrammingIcon"
 
+interface toDisplay {
+  label: string
+}
+
+const itemsToDisplay: Array<toDisplay> = [
+  { label: "Competitive Salary" },
+  { label: "Flexibility" },
+  { label: "Impact and Contribution" },
+  { label: "Financial Freedom" },
+  { label: "Job Satisfaction" },
+  { label: "Global Reach" },
+]
+
 function index() {
   return (
     <div className="w-full h-full bg-white flex items-center justify-center">
-      <div className="w-full h-full bg-white flex items-center justify-center">
-        <div className="w-1/2 text-black font-bold h-full">
+      <div className="w-full h-100 bg-white md:flex items-center justify-center">
+        <div className="md:w-1/2 w-full text-black font-bold h-full">
           <div className="w-full h-full flex flex-col items-center justify-center">
             <div
-              style={{ fontSize: "3.6rem" }}
+              style={{ fontSize: "3.4rem" }}
               className="w-full h-full flex items-center justify-center"
             >
               <div className="flex flex-col justify-center items-center">
@@ -35,14 +48,16 @@ function index() {
             </div>
           </div>
         </div>
-        <div className="w-1/2 h-full">
+        <div className="md:w-1/2 w-full h-full">
           <div>
-            <MovingBlob
-              label="xyz"
-              icon={<ProgrammingIcon />}
-              className=" mt-24 w-64 bg-slate-600"
-              labelStyles={{ color: "black" }}
-            />
+            {itemsToDisplay.map((x) => (
+              <MovingBlob
+                label={x.label}
+                icon={<ProgrammingIcon />}
+                className=" mt-24 w-40  z-50"
+                labelStyles={{ color: "black" }}
+              />
+            ))}
           </div>
         </div>
       </div>
