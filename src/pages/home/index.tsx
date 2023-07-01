955a2a77d89381e4704e9419d7e2b88c599969e3
Aleksy <psychologianauki@gmail.com>
2023-07-01 19:54:31 +0200

    done for today

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -19,8 +19,11 @@ const itemsToDisplay: Array<toDisplay> = [
 
 function index() {
   return (
-    <div className="w-full h-full bg-white flex items-center justify-center">
-      <div className="w-full h-100 bg-white md:flex items-center justify-center">
+    <div
+      className="w-full h-full bg-white flex "
+      style={{ height: "100%", backgroundColor: "white" }}
+    >
+      <div className="w-full h-full bg-white md:flex mt-5">
         <div className="md:w-1/2 w-full text-black font-bold h-full">
           <div className="w-full h-full flex flex-col items-center justify-center">
             <div
@@ -48,16 +51,29 @@ function index() {
             </div>
           </div>
         </div>
-        <div className="md:w-1/2 w-full h-full">
-          <div>
-            {itemsToDisplay.map((x) => (
-              <MovingBlob
-                label={x.label}
-                icon={<ProgrammingIcon />}
-                className=" mt-24 w-40  z-50"
-                labelStyles={{ color: "black" }}
-              />
-            ))}
+        <div className="md:w-1/2 w-full h-full flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center">
+            <div
+              style={{
+                color: "black",
+                fontWeight: "bolder",
+                fontSize: "20px",
+                marginTop: "20px",
+              }}
+            >
+              What you will get
+              <br /> if you will make a decision right now:
+            </div>
+            <div>
+              {itemsToDisplay.map((x) => (
+                <MovingBlob
+                  label={x.label}
+                  icon={<ProgrammingIcon width="40" height="40" />}
+                  className="mt-20 w-2 h-2 z-50 "
+                  labelStyles={{ color: "black", fontSize: "10px" }}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
